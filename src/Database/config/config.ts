79b508cf4fcc22183afeb175e3e110079e431380
Anderson <omicron_rpl@hotmail.com>
2022-07-15 17:11:14 -0300

    build(docker): added template_dev, template_test, template_prod databases creation on docker compose

--- src/Database/config/config.ts
+++ src/Database/config/config.ts
@@ -6,21 +6,21 @@ module.exports =
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_SCHEMA,
+    "database": process.env.DB_DEV,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_SCHEMA,
+    "database": process.env.DB_TEST,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_SCHEMA,
+    "database": process.env.DB_PROD,
     "host": process.env.DB_HOST,
     "dialect": process.env.DB_DIALECT
   }
